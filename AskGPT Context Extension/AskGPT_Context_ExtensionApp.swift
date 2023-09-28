d7ea3011b8082f24abfef7bb2248d6e6e73dc5de
Niral Patel <patnir41@gmail.com>
2023-09-28 00:41:56 -0700

    Initial Commit

---
+++ AskGPT Context Extension/AskGPT_Context_ExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskGPT_Context_ExtensionApp.swift
+//  AskGPT Context Extension
+//
+//  Created by Niral Patel on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AskGPT_Context_ExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
